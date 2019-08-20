1f745a2f436fad449413430ae2ea5d8eb0da8092
John Karrell Speck <speckworks@gmail.com>
2019-08-20 03:09:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,26 +1,22 @@
 require "pry"
 require "yaml"
 
-def load_library
-emoticons = YAML.load_file('emoticons.yml')
+def load_library(emo_icon)
+emoticon_hashsource = YAML.load_file("./lib/emoticons.yml")
+emoticons =  {
+  'get_meaning' => {},
+  'get_emoticon' => {}
+}
+emoticon_hashsource.each do |key, val|
+  emoticons['get_emoticon'][val[0]] = emoticon_hashsource[key][1]
+    emoticons['get_meaning'][val[1]] =key
 end
-
+emoticons
+end
+  
+  
 def get_japanese_emoticon(emoticons)
-  emoticons = YAML.load_file(emoticon-translator-lab-dumbo-web-82619/lib/'emoticons.yml')
-  emo_dic={}
-  emoticons.each do |emoticon , japanese_version|
-  japanese_version.each do |emo_jap,emo_english|
-  if !emo_dic[emo_english]
-        emo_dic[emo_english]={}
-      end
-      if !emo_dic[emo_english][emoticon]
-        emo_dic[emo_english][emoticon]=[]
-      end
-      emo_dic[japanese_version][emoticon]<< emo_jap.to_s 
-    end
-  emo_dic
-end
-binding.pry
+ 
 
 def get_english_meaning
   emo_dic[:japanese_version]
